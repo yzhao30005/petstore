231005bd7184a72eb96572a8a47de246b4c69c5e
Yan Zhao <yan.zhao@aciworldwide.com>
2025-06-15 22:18:19 -0400

    jnit test

--- src/test/java/com/petstore/petstore/PetstoreApplicationTests.java
+++ src/test/java/com/petstore/petstore/PetstoreApplicationTests.java
@@ -8,19 +8,34 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
 import com.petstore.petstore.controller.PetstoreController;
+import com.petstore.petstore.controller.repository.ProductRepository;
+import com.petstore.petstore.model.Product;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class PetstoreApplicationTests {
 
   @Autowired
-  protected MockMvc mockMvc;
+  private MockMvc mockMvc;
+
+  @Autowired
+  private ProductRepository productRepository;
 
 	@Test
 	void testGetProduct() throws Exception {
+	  Product product = new Product();
+	  product.setName("test");
+	  product.setDescription("This is a test");
+	  product.setQuantity(2);
+
+	  productRepository.save(product);
+
     mockMvc
-    .perform(get(PetstoreController.BASE_URL).contentType(APPLICATION_JSON)        )
+    .perform(get(PetstoreController.BASE_URL)
+        .contentType(APPLICATION_JSON)        )
     .andExpect(status().isOk());
 	}
 
